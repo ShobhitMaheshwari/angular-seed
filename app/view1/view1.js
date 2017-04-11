b7a375ca753f59dd69daf19f4663fe43c090b577
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-04-11 15:18:31 -0700

    temp

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,19 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['DataService', 'plotData', function(DataService, plotData) {
 
+	console.log("initialization");
+	//var I = getIntervalI(100000001)
+	//var J = getIntervalJ(100001)
+	var I = DataService.getIntervalI(1000001)
+	var J = DataService.getIntervalJ(1001)
+	console.log("end");
+
+	var overlap = plotData.getOverlapIntervals(I, J[0]);
+
+	var bin = plotData.getFrequencyVector(overlap, J[0]);
+	console.log(bin);
 }]);
+
+
