2a2071b2b5563f27731b658f3e77135b2bd7d27b
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-04-12 22:15:13 -0700

    better chart

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -29,7 +29,6 @@ angular.module('myApp.view2', ['ngRoute'])
 				"frequency": bin[i]
 			});
 		}
-		console.log(bin.length);
 		return data;
 	}
 
@@ -52,46 +51,73 @@ angular.module('myApp.view2', ['ngRoute'])
 
 	$scope.loaded = false;
 
+	var offset = (new Date(2017,4,15)).getTime();
+
+	console.log(offset);
 	dataservice.getDataAsync().then(function(data){
-	var svg = d3.select("svg"),
-    	margin = {top: 20, right: 20, bottom: 30, left: 40},
-	    width = +svg.attr("width") - margin.left - margin.right,
-    	height = +svg.attr("height") - margin.top - margin.bottom;
+		data.forEach(function(d, i){
+			d.letter = new Date(d.letter*1000*3600 + offset);
+			if(i%24==0)console.log(d.letter);
+		});
+		console.log(d3.max(data, function(d) { return d.letter; }));
 
-	var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+
+	var	margin = {top: 20, right: 20, bottom: 60, left: 40};
+
+	var svg = d3.select("svg").append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+		svg1 = d3.select("svg"),
+	    width = +svg1.attr("width") - margin.left - margin.right,
+    	height = +svg1.attr("height") - margin.top - margin.bottom;
+
+	var	x = d3.scaleTime().rangeRound([0, width]),
     	y = d3.scaleLinear().rangeRound([height, 0]);
-
-	var g = svg.append("g")
-    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-	x.domain(data.map(function(d) { return d.letter; }));
+	x.domain([d3.min(data, function(d) { return d.letter; }), d3.max(data, function(d) { return d.letter; })]);
 	y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-	g.append("g")
-		.attr("class", "axis axis--x")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
 
-	g.append("g")
-		.attr("class", "axis axis--y")
-		.call(d3.axisLeft(y).ticks(10, "%"))
-		.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("Frequency");
+	var xAxis = d3.axisBottom()
+    	.scale(x)
+		.tickFormat(d3.timeFormat("%H:%M %a"))
+		.ticks(d3.timeHour.every(12));
 
-	g.selectAll(".bar")
+	var yAxis = d3.axisLeft()
+    	.scale(y)
+    	.ticks(10);
+
+	svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "middle")
+      .attr("dx", "-3em")
+      .attr("dy", ".75em")
+      .attr("transform", "rotate(-60)" );
+
+  	svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Value ($)");
+
+	svg.selectAll(".bar")
 		.data(data)
 		.enter().append("rect")
 		.attr("class", "bar")
 		.attr("x", function(d) { return x(d.letter); })
 		.attr("y", function(d) { return y(d.frequency); })
-		.attr("width", x.bandwidth())
+		.attr("width", function(d, i){
+			if(i != data.length - 1){
+				return x(data[i+1].letter) - x(data[i].letter);
+			}else
+				return width - x(data[i].letter);
+		})
 		.attr("height", function(d) { return height - y(d.frequency); });
-
 	$scope.loaded = true;
 	});
 
